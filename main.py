545ba5a404756927ba4a5669f5e2a228d956bbbf
Justin White <jw@justinwhite.net>
2016-06-09 00:25:50 -0400

    Initial commit. Plain page, no caching.

---
+++ main.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+import calendar
+import json
+import time
+import urllib2
+
+import webapp2
+
+api_key = open("api_key.txt").readline().strip()
+
+
+def fetch_and_decode():
+    out = ""
+
+    request_url = "https://www.worldtides.info/api?{options}&lat={lat}&lon={lon}&start={start}&key={key}\n".format(
+        options="extremes",
+        # Lynn, MA
+        lat=42.4787444,
+        lon=-71.0011876,
+        # right now in UTC as seconds since epoch
+        start=calendar.timegm(time.gmtime()),
+        key=api_key
+    )
+    out += request_url
+
+    response = urllib2.urlopen(request_url)
+    data = json.loads(response.read().decode())
+    # out += data
+
+    out += data['copyright'] + "\n"
+
+    out += "Location: %s, %s\n" % (data['responseLat'], data['responseLon'])
+
+    for tide in data['extremes']:
+        out += "{type}\t{time}\n".format(
+            type=tide['type'],
+            time=time.asctime(time.localtime(tide['dt']))
+        )
+
+    return out
+
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("""
+<html>
+<head>
+<title>tide-cacher</title>
+</head>
+<body>
+<pre>
+%s
+</pre>
+</body>
+</html>
+""" % fetch_and_decode()
+                            )
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
