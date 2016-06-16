bcd6c8286b09173cfb04210262dc59415b36547d
Justin White <jw@justinwhite.net>
2016-06-16 00:41:54 -0400

    Use freshly API-ified worldtides_info.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,6 @@
-#!/usr/bin/env python
-
-import calendar
-import json
-import time
-import urllib2
-
 import webapp2
 
-api_key = open("api_key.txt").readline().strip()
+import worldtides_info as tides
 
 template = """
 <html>
@@ -22,41 +15,16 @@ template = """
 </html>
 """
 
-
-def fetch_and_decode():
-    out = ""
-
-    request_url = "https://www.worldtides.info/api?{options}&lat={lat}&lon={lon}&start={start}&key={key}\n".format(
-        options="extremes",
-        # Lynn, MA
-        lat=42.4787444,
-        lon=-71.0011876,
-        # right now in UTC as seconds since epoch
-        start=calendar.timegm(time.gmtime()),
-        key=api_key
-    )
-    out += request_url
-
-    response = urllib2.urlopen(request_url)
-    data = json.loads(response.read().decode())
-    # out += data + "\n"
-
-    out += data['copyright'] + "\n"
-
-    out += "Location: %s, %s\n" % (data['responseLat'], data['responseLon'])
-
-    for tide in data['extremes']:
-        out += "{type:>4}  {time}\n".format(
-            type=tide['type'],
-            time=time.asctime(time.localtime(tide['dt']))
-        )
-
-    return out
+api_key = tides.get_api_key()
 
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write(template % fetch_and_decode())
+        output = ""
+        url, data = tides.fetch(api_key)
+        output += url + '\n'
+        output += tides.decode(data)
+        self.response.write(template % output)
 
 
 app = webapp2.WSGIApplication([
---
+++ app/worldtides_info.py
@@ -0,0 +1,59 @@
+import calendar
+import json
+import time
+import urllib
+
+
+def get_api_key():
+    return open("worldtides_api_key.txt").readline().strip()
+
+
+def fetch(api_key):
+    # Lynn, MA
+    origin_lat = 42.478744
+    origin_lon = -71.001188
+    origin_location = (origin_lat, origin_lon)
+
+    request_url = "https://www.worldtides.info/api?{options}&lat={lat}&lon={lon}&start={start}&key={key}\n".format(
+        options="extremes",
+        lat=origin_lat,
+        lon=origin_lon,
+        # right now in UTC as seconds since epoch
+        start=calendar.timegm(time.gmtime()),
+        key=api_key
+    )
+    response = urllib.urlopen(request_url)
+
+    return request_url, response.read()
+
+
+def decode(data):
+    data = json.loads(data)
+    out = ""
+
+    try:
+        out += data['copyright'] + "\n"
+
+        result_lat = data['responseLat']
+        result_lon = data['responseLon']
+
+        out += "Location: %s, %s\n" % (result_lat, result_lon)
+
+        for tide in data['extremes']:
+            out += "{type:>4}  {time}\n".format(
+                type=tide['type'],
+                time=time.asctime(time.localtime(tide['dt']))
+            )
+    except KeyError:
+        out += "Error: Bad JSON Data\n"
+        out += str(data) + '\n'
+
+    return out
+
+
+if __name__ == "__main__":
+    key = get_api_key()
+    url, response_data = fetch(key)
+    print(url)
+    out = decode(response_data)
+    print(out)
