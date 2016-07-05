ad94db5fc5f5e7cfcb25a6cf54f98d24366bc3a8
Justin White <jw@justinwhite.net>
2016-07-05 18:12:46 -0400

    Update utils to use naive datetime instead of 'unix time as integer'

--- app/utils.py
+++ app/utils.py
@@ -2,7 +2,6 @@ import datetime
 
 
 def adjusted_datetime(timestamp, tz_offset):
-    epoch_time = datetime.datetime.utcfromtimestamp(timestamp)
     offset_delta = datetime.timedelta(hours=tz_offset)
-    adjusted_timestamp = epoch_time + offset_delta
+    adjusted_timestamp = timestamp + offset_delta
     return adjusted_timestamp
