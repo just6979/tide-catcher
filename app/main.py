60db428e08f23f8867944957a0db20a070dc6f41
Justin White <jw@justinwhite.net>
2016-07-10 13:44:46 -0400

    Stub out caching functions.

--- app/main.py
+++ app/main.py
@@ -19,17 +19,35 @@ maps_api_key = maps_api.get_api_key()
 
 
 class TideData(ndb.Model):
-    result_location = ndb.StringProperty(indexed=True)
+    req_loc = ndb.StringProperty(indexed=True)
+    req_time = ndb.TimeProperty(indexed=False, required=True)
     tide_data = ndb.JsonProperty(indexed=False, required=True)
 
 
 class LocationMatch(ndb.Model):
-    query_location = ndb.StringProperty(indexed=True)
-    result_location = ndb.StringProperty(indexed=False, required=True)
+    req_location = ndb.StringProperty(indexed=True)
+    resp_location = ndb.StringProperty(indexed=False, required=True)
+
+
+def nearest_station_loc(req_loc):
+    return req_loc
+
+
+def save_nearest_station(req_loc):
+    pass
+
+
+def check_cache(location, save_time):
+    return False
+
+
+def save_to_cache(location, utc_now):
+    pass
 
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        # TODO: cache & reuse requests for the same station within 12 hours
         # TODO: handle incoming location requests (Full page response or HttpReq and JSON?)
         # Lynn, MA
         req_lat = 42.478744
@@ -41,41 +59,52 @@ class MainHandler(webapp2.RequestHandler):
         # minus 12 hours to make sure we get the last tide (maybe last 2)
         utc_minus_12 = utc_now + datetime.timedelta(hours=-12)
 
-        (tides, tides_url), (tz, tz_url) = tides_api.fetch_and_decode(
-            tides_api_key, maps_api_key, req_loc, utc_minus_12, utc_now
-        )
+        station_loc = nearest_station_loc(req_loc)
 
-        if 'error' in tz:
-            values = {
-                'error': tz['error'],
-                'data': tz['data'],
-            }
-        elif 'error' in tides:
-            values = {
-                'error': tides['error'],
-                'data': tides['data'],
-            }
-        else:
-            logging.info(tz_url)
-            logging.info(tides_url)
-            start_timestamp = to_nearest_minute(offset_timestamp(utc_now, tz['offset']))
-            req_timestamp = {
-                'date': start_timestamp.strftime(DATE_FORMAT),
-                'time': start_timestamp.strftime(TIME_FORMAT),
-                'day': start_timestamp.strftime(DAY_FORMAT),
-            }
-            values = {
-                'copyright': tides['copyright'],
-                'req_timestamp': req_timestamp,
-                'req_lat': req_lat,
-                'req_lon': req_lon,
-                'resp_lat': tides['lat'],
-                'resp_lon': tides['lon'],
-                'resp_station': tides['station'],
-                'tz_offset': tz['offset'],
-                'tz_name': tz['name'],
-                'tides': tides['tides'],
-            }
+        # check if this location was cached in the last 12 hours
+        values = check_cache(station_loc, utc_minus_12)
+
+        if not values:
+            # if this location wasn't cached
+            (tides, tides_url), (tz, tz_url) = tides_api.fetch_and_decode(
+                tides_api_key, maps_api_key, req_loc, utc_minus_12, utc_now
+            )
+
+            station_loc = (tides['lat'], tides['lon'])
+
+            if 'error' in tz:
+                values = {
+                    'error': tz['error'],
+                    'data': tz['data'],
+                }
+            elif 'error' in tides:
+                values = {
+                    'error': tides['error'],
+                    'data': tides['data'],
+                }
+            else:
+                logging.info(tz_url)
+                logging.info(tides_url)
+                start_timestamp = to_nearest_minute(offset_timestamp(utc_now, tz['offset']))
+                req_timestamp = {
+                    'date': start_timestamp.strftime(DATE_FORMAT),
+                    'time': start_timestamp.strftime(TIME_FORMAT),
+                    'day': start_timestamp.strftime(DAY_FORMAT),
+                }
+                values = {
+                    'copyright': tides['copyright'],
+                    'req_timestamp': req_timestamp,
+                    'req_lat': req_lat,
+                    'req_lon': req_lon,
+                    'resp_lat': tides['lat'],
+                    'resp_lon': tides['lon'],
+                    'resp_station': tides['station'],
+                    'tz_offset': tz['offset'],
+                    'tz_name': tz['name'],
+                    'tides': tides['tides'],
+                }
+
+            save_to_cache(station_loc, utc_now)
 
         template = JINJA_ENVIRONMENT.get_template("index.html")
         self.response.write(template.render(values))
