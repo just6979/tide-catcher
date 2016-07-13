26fe113fcbbcc95f9d96ae91965827232b737336
Justin White <jw@justinwhite.net>
2016-07-12 23:29:20 -0400

    Move templates to their own directory.

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ import worldtides_info as tides_api
 from utils import *
 
 JINJA_ENVIRONMENT = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
