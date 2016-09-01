15f9b26a38fae27a4747fe8c2fbf55e5674cf0ac
Justin White <jw@justinwhite.net>
2016-09-01 04:49:02 -0400

    Get rid of the TestHandlers, since we're moving away from server-side templating.
    Also merge JSON handlers back into main handlers module.

--- app/handlers.py
+++ app/handlers.py
@@ -1,3 +1,5 @@
+import json
+
 import webapp2
 
 import templates
@@ -30,17 +32,31 @@ class StationRefreshHandler(webapp2.RequestHandler):
             return templates.render(self, template_file, values)
 
 
-class TidesLynnHandler(webapp2.RequestHandler):
+class JSONTidesHandler(webapp2.RequestHandler):
     def get(self):
-        # Lynn, MA
-        req_lat = 42.478744
-        req_lon = -71.001188
-
-        values = tides.for_location((req_lat, req_lon))
-
-        if values['status'] == 'OK':
-            template_file = 'tides_lynn.html'
+        location = self.request.get(u'loc', default_value=None)
+        if location:
+            try:
+                req_lat, req_lon = location.split(',')
+            except ValueError as e:
+                self.response.write(
+                    u'Bad Location: "%s", %s' % (location, e.message)
+                )
+                self.response.set_status(404)
+            except Exception as e:
+                self.response.write(
+                    u'Bad Location: "%s", %s' % (location, e.message)
+                )
+                self.response.set_status(404)
+            else:
+                values = tides.for_location((req_lat, req_lon))
+                self.response.write(json.dumps(values))
         else:
-            template_file = 'error.html'
+            self.response.write(u'No Location Given')
+            self.response.set_status(404)
 
-        templates.render(self, template_file, values)
+
+class JSONStationsHandler(webapp2.RequestHandler):
+    def get(self):
+        values = tides.get_stations()
+        self.response.write(json.dumps(values))
--- app/main.py
+++ app/main.py
@@ -1,27 +1,14 @@
 import webapp2
 
 import handlers
-import json_handlers
-import test_handlers
 
 app = webapp2.WSGIApplication([
-
     ('/', handlers.IndexHandler),
     ('/tides', handlers.TidesHandler),
 
     ('/stations', handlers.StationsHandler),
     ('/refresh-stations', handlers.StationRefreshHandler),
 
-    # the old default: non-AJAX, non geolocated
-    ('/lynn', handlers.TidesLynnHandler),
-
-    ('/json/tides', json_handlers.JSONTidesHandler),
-    ('/json/stations', json_handlers.JSONStationsHandler),
-
-    ('/test', test_handlers.TestHandler),
-    ('/test/base', test_handlers.BaseTestHandler),
-    ('/test/error', test_handlers.ErrorTestHandler),
-    ('/test/tides', test_handlers.TidesTestHandler),
-    ('/test/stations', test_handlers.StationsTestHandler),
-
+    ('/json/tides', handlers.JSONTidesHandler),
+    ('/json/stations', handlers.JSONStationsHandler),
 ], debug=True)
